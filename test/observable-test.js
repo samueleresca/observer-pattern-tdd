8174c736ac0a4965af9d1d86f1ce9aeb4c6fe482
Samuele Resca <samuele.resca@gmail.com>
2016-02-14 16:40:17 +0100

    Rework

--- test/observable-test.js
+++ test/observable-test.js
@@ -19,22 +19,21 @@ describe('ObservableAddObserverTest', function () {
 
 });
 
+describe('ObservableHasObserverTest', function () {
+    before(function () { });
 
-describe('ObservableHasObserverTest',function(){
-   before(function(){});
-   
-   it("test should return true when has observer",function(){
-      var observable = new tddjs.util.Observable();
-      var observer= function(){};
-      
-      observable.addObserver(observer);
-      expect(observable.hasObserver(observer)).to.be.ok; 
-   });
-   
-    it("test should return false when has no observer",function(){
-      var observable = new tddjs.util.Observable();
-      var observer= function(){};
-      
-      expect(observable.hasObserver(observer)).to.be.not.ok; 
-   });
+    it("test should return true when has observer", function () {
+        var observable = new tddjs.util.Observable();
+        var observer = function () { };
+
+        observable.addObserver(observer);
+        expect(observable.hasObserver(observer)).to.be.ok;
+    });
+
+    it("test should return false when has no observer", function () {
+        var observable = new tddjs.util.Observable();
+        var observer = function () { };
+
+        expect(observable.hasObserver(observer)).to.be.not.ok;
+    });
 });
